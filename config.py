5dfd2b4046b76161ea5e83ab1f1ef81f04371f68
Thân Trọng Duy <22730070@ms.uit.edu.vn>
2025-05-09 17:50:11 +0700

    tường
    Co-Authored-By: Thân Trọng Duy <93548856+Thitrongdan202@users.noreply.github.com>
    Co-Authored-By: 22730064_Nguyễn Hoàng Anh <141211658+HoangAnh22730064@users.noreply.github.com>
    Co-Authored-By: Nguyen Thanh Thinh <22730096@ms.uit.edu.vn>

--- config.py
+++ config.py
@@ -10,9 +10,12 @@ SCREEN_HEIGHT = CELL_HEIGHT * CELL_SIZE
 GREEN = (0, 255, 0)        # rắn
 RED   = (255, 0, 0)        # mồi
 BG_COLOR   = (30, 30, 30)  # nền
-WALL_COLOR = (0, 255, 255) # viền (cyan)
+
 
 # ───── CẤU HÌNH GAME ─────
 FPS            = 10         # tốc độ khung hình
 POINTS_TO_GROW = 3         # đủ 3 điểm rắn dài 1 đoạn
+
+# Cấu hình tường 
+WALL_COLOR = (0, 255, 255) # viền (cyan)
 WALL_THICKNESS = 7         # độ dày nét viền (px)
